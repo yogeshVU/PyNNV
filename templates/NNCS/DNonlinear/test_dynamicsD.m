2c836ca4182bf0b1d831850fc3b1920ef4026d36
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2020-09-15 16:09:52 -0500

    Updated the files to match the CORA release: https://github.com/dieman95/nnv/commit/1294a99dc9fc25514b40ef5effcdb6f012e02280

--- templates/NNCS/DNonlinear/test_dynamicsD.m
+++ templates/NNCS/DNonlinear/test_dynamicsD.m
@@ -1,4 +1,4 @@
-function [dx]=test_dynamics(t,x, a_ego,T)
+function [dx]=test_dynamicsD(x, a_ego)
 
 mu=0.0001; % friction parameter
 
@@ -13,12 +13,12 @@ T = [];
 % u = a_ego: control input
 
 % lead car dynamics
-a_lead = -2; 
+a_lead = -2;
 dx(1,1)=x(2);
 dx(2,1) = x(3);
 dx(3,1) = -2 * x(3) + 2 * a_lead - mu*x(2)^2;
 % ego car dyanmics
-dx(4,1)= x(5); 
+dx(4,1)= x(5);
 dx(5,1) = x(6);
 dx(6,1) = -2 * x(6) + 2 * a_ego - mu*x(5)^2;
 end
