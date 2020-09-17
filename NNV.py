82bec4d90c4fc0db1084272171a796fb3a88d5ee
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2020-09-17 09:44:59 -0500

    update to be compatible with the py3.6

--- NNV.py
+++ NNV.py
@@ -3,139 +3,139 @@
 
 # It is going to load the json file and invoke appropriate functions..
 
-from __future__ import annotations
-from abc import ABC, abstractmethod
-from typing import List
+# from __future__ import annotations
+# from abc import ABC, abstractmethod
+# from typing import List
 
-
-class NNV():
-    """
-    The Context defines the interface of interest to clients.
-    """
-
-    def __init__(self, strategy: Strategy) -> None:
-        """
-        Usually, the Context accepts a strategy through the constructor, but
-        also provides a setter to change it at runtime.
-        """
-
-        self._strategy = strategy
-
-    @property
-    def strategy(self) -> Strategy:
-        """
-        The Context maintains a reference to one of the Strategy objects. The
-        Context does not know the concrete class of a strategy. It should work
-        with all strategies via the Strategy interface.
-        """
-
-        return self._strategy
-
-    @strategy.setter
-    def strategy(self, strategy: Strategy) -> None:
-        """
-        Usually, the Context allows replacing a Strategy object at runtime.
-        """
-
-        self._strategy = strategy
-
-    def do_reachability(self) -> None:
-        """
-        The Context delegates some work to the Strategy object instead of
-        implementing multiple versions of the algorithm on its own.
-        """
-
-        # ...
-
-        print("Context: do_reachability using strategy")
-        result = self._strategy.do_reachability()
-        return result
-
-    def do_verification(self) -> None:
-        """
-        The Context delegates some work to the Strategy object instead of
-        implementing multiple versions of the algorithm on its own.
-        """
-        # ...
-
-        print("Context: do_reachability using strategy")
-        result = self._strategy.do_verification()
-        return result
-    
-
-    def do_algorithm(self) -> None:
-        """
-        The Context delegates some work to the Strategy object instead of
-        implementing multiple versions of the algorithm on its own.
-        """
-        # ...
-
-        print("Context: do_algorithm using strategy")
-        result = self._strategy.do_algorithm()
-        return result
-        # ...
-
-
-class Strategy(ABC):
-    """
-    The Strategy interface declares operations common to all supported versions
-    of some algorithm.
-
-    The Context uses this interface to call the algorithm defined by Concrete
-    Strategies.
-    """
-
-    @abstractmethod
-    def do_algorithm(self):
-        pass
-
-    @abstractmethod
-    def do_reachability(self):
-        pass
-    
-    @abstractmethod
-    def do_verification(self):
-        pass
-
-    @abstractmethod
-    def do_parse(self):
-        pass
-    
-
-"""
-Concrete Strategies implement the algorithm while following the base Strategy
-interface. The interface makes them interchangeable in the Context.
-"""
-
-
-class ConcreteStrategyA(Strategy):
-    def do_algorithm(self):
-        return print("ConcreteStrategyA")
-
-    def do_parse(self):
-        pass
-
-    def do_reachability(self):
-        pass
-
-    def do_verification(self):
-        pass
-
-
-
-
-class ConcreteStrategyB(Strategy):
-    def do_algorithm(self):
-        return print("ConcreteStrategyB")
-
-    def do_parse(self):
-        pass
-
-    def do_reachability(self):
-        pass
-
-    def do_verification(self):
-        pass
+#
+# class NNV():
+#     """
+#     The Context defines the interface of interest to clients.
+#     """
+#
+#     def __init__(self, strategy: Strategy) -> None:
+#         """
+#         Usually, the Context accepts a strategy through the constructor, but
+#         also provides a setter to change it at runtime.
+#         """
+#
+#         self._strategy = strategy
+#
+#     @property
+#     def strategy(self) -> Strategy:
+#         """
+#         The Context maintains a reference to one of the Strategy objects. The
+#         Context does not know the concrete class of a strategy. It should work
+#         with all strategies via the Strategy interface.
+#         """
+#
+#         return self._strategy
+#
+#     @strategy.setter
+#     def strategy(self, strategy: Strategy) -> None:
+#         """
+#         Usually, the Context allows replacing a Strategy object at runtime.
+#         """
+#
+#         self._strategy = strategy
+#
+#     def do_reachability(self) -> None:
+#         """
+#         The Context delegates some work to the Strategy object instead of
+#         implementing multiple versions of the algorithm on its own.
+#         """
+#
+#         # ...
+#
+#         print("Context: do_reachability using strategy")
+#         result = self._strategy.do_reachability()
+#         return result
+#
+#     def do_verification(self) -> None:
+#         """
+#         The Context delegates some work to the Strategy object instead of
+#         implementing multiple versions of the algorithm on its own.
+#         """
+#         # ...
+#
+#         print("Context: do_reachability using strategy")
+#         result = self._strategy.do_verification()
+#         return result
+#
+#
+#     def do_algorithm(self) -> None:
+#         """
+#         The Context delegates some work to the Strategy object instead of
+#         implementing multiple versions of the algorithm on its own.
+#         """
+#         # ...
+#
+#         print("Context: do_algorithm using strategy")
+#         result = self._strategy.do_algorithm()
+#         return result
+#         # ...
+#
+#
+# class Strategy(ABC):
+#     """
+#     The Strategy interface declares operations common to all supported versions
+#     of some algorithm.
+#
+#     The Context uses this interface to call the algorithm defined by Concrete
+#     Strategies.
+#     """
+#
+#     @abstractmethod
+#     def do_algorithm(self):
+#         pass
+#
+#     @abstractmethod
+#     def do_reachability(self):
+#         pass
+#
+#     @abstractmethod
+#     def do_verification(self):
+#         pass
+#
+#     @abstractmethod
+#     def do_parse(self):
+#         pass
+#
+#
+# """
+# Concrete Strategies implement the algorithm while following the base Strategy
+# interface. The interface makes them interchangeable in the Context.
+# """
+#
+#
+# class ConcreteStrategyA(Strategy):
+#     def do_algorithm(self):
+#         return print("ConcreteStrategyA")
+#
+#     def do_parse(self):
+#         pass
+#
+#     def do_reachability(self):
+#         pass
+#
+#     def do_verification(self):
+#         pass
+#
+#
+#
+#
+# class ConcreteStrategyB(Strategy):
+#     def do_algorithm(self):
+#         return print("ConcreteStrategyB")
+#
+#     def do_parse(self):
+#         pass
+#
+#     def do_reachability(self):
+#         pass
+#
+#     def do_verification(self):
+#         pass
 
 
 
@@ -173,7 +173,7 @@ class NNVExec:
         print("The current strategy is:", strategy)
 
         eng.cd(input_dir_path)
-
+        context = None
         if strategy == NNVKeys.template_NN_CNN_key:
             context = CNN(eng)
         elif strategy == NNVKeys.template_NN_FFNN_key:
